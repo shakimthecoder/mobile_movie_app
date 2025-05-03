ec0a93920ed13de55aa81a731bd144c088d11e2f
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:48:03 -0400

    add dynamic movie route

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <Text>Profile Page</Text>
+        </View>
+    )
+}
+
+export default MovieDetails;
+const styles = StyleSheet.create({});
