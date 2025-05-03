d2fffad3e2df4506def263ba524ca2d86e156c9f
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:34:17 -0400

    create onboarding route

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-blue-500 font-bold">Welcome to my app</Text>
+        <Link href="/onboarding">Onboarding</Link>
         <Text className="text-[#123123] p-5">Register</Text>
         <Text className="text-primary p-5">Login</Text>
         <Text className="text-accent">Copyright 2025</Text>
