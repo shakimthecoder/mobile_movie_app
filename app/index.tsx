7ad1bee5bf51a193cf240b105a0137566d12576e
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:07:39 -0400

    add primary style

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ export default function Index() {
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-blue-500 font-bold">Welcome to my app</Text>
         <Text className="text-[#123123] p-5">Register</Text>
+        <Text className="text-primary p-5">Login</Text>
     </View>
   );
 }
