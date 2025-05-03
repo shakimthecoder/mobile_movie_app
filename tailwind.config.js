9733ad1d298b70b6b20dc5d29522463ef7355c97
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:14:00 -0400

    add light colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     extend: {
       colors: {
         primary: '#030014',
+        secondary: '#151312',
         accent: '#AB8BFF',
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '',
+          200: '',
+          300: '',
+        },
       }
     },
   },
