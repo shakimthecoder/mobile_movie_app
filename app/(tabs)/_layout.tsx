f88c008d9b56c9687c6dd999bbab44b385b7618e
Shakim Harris <amenraprod@gmail.com>
2025-05-03 00:40:05 -0400

    style tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,30 +4,45 @@ import { ImageBackground } from 'react-native';
 import { images } from '@/constants/images';
 import { icons } from '@/constants/icons';
 
-
+const TabIcon = ({ icon, title, focused } : any) => {
+    if (focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full
+                      flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center
+                      round-full overflow-hidden">
+                <Image
+                    source={icon}
+                    tintColor="#151312"
+                    className="size-5"
+                />
+                <Text className="text-secondary text-base ml-2 font-semibold">{title}</Text>
+            </ImageBackground>
+        );
+    }
+    return (
+        <View className="size-full justify-center items-center">
+            <Image source={icon} tintColor="#A8B5DB" className="size-5"></Image>
+        </View>
+    )
+}
 const _Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+         screenOptions={{ tabBarShowLabel: false }}
+        >
             <Tabs.Screen
              name="index"
              options={{
                  title: 'Home',
                  headerShown: false,
                  tabBarIcon: ({ focused }) => (
-                     <>
-                     <ImageBackground
-                      source={images.highlight}
-                      className="flex flex-row w-full
-                      flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center
-                      round-full overflow-hidden">
-                         <Image
-                         source={icons.home}
-                         tintColor="#151312"
-                         className="size-5"
-                         />
-                         <Text>Home</Text>
-                     </ImageBackground>
-                     </>
+                     <TabIcon
+                      focused={focused}
+                      icon={icons.home}
+                      title="Home"
+                     />
                  )
             }}
             />
@@ -35,19 +50,42 @@ const _Layout = () => {
                 name="profile"
                 options={{
                     title: 'Profile',
-                    headerShown: false }}
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.profile}
+                            title="Profile"
+                        />
+                    )
+            }}
             />
             <Tabs.Screen
                 name="saved"
                 options={{
                     title: 'Saved',
-                    headerShown: false }}
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Saved"
+                        />
+                    )
+            }}
             />
             <Tabs.Screen
                 name="search"
                 options={{
                     title: 'Search',
-                    headerShown: false }}
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
+                        />
+                    )}}
             />
         </Tabs>
     )
