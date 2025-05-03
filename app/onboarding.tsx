c40f52f7e79e0b3c57a90e390d0ec18a1d4876f4
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:31:01 -0400

    add onboarding page

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Onboarding = () => {
+    return (
+        <View>
+            <Text>Onboarding Page</Text>
+        </View>
+    )
+}
+
+export default Onboarding;
+const styles = StyleSheet.create({});
