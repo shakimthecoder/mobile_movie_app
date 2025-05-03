afe9761683a0fc9cace84531235356fe09105136
Shakim Harris <amenraprod@gmail.com>
2025-05-02 22:46:33 -0400

    added save and search screen

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Search = () => {
+    return (
+        <View>
+            <Text>Search Page</Text>
+        </View>
+    )
+}
+
+export default Search;
+const styles = StyleSheet.create({});
