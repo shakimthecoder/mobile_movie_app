38b7c5ab5670f559680934829b7ce0672381e2d2
Shakim Harris <amenraprod@gmail.com>
2025-05-05 04:19:04 -0400

    add input to searchbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import { Text, View, ScrollView } from "react-native";
 import { Link } from "expo-router";
+import SearchBar from "../components/SearchBar";
 
 export default function Index() {
   return (
     <View className="flex-1 bg-primary">
       <ScrollView className="flex-1 px-5">
+        <View className="flex-1 mt-5">
+          <SearchBar />
+        </View>
       </ScrollView>
     </View>
   );
---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import { Text, View, ScrollView, TextInput, Image } from "react-native";
+import { icons } from "@/constants/icons";
+
+export default function SearchBar() {
+    return (
+      <View className="flex-row items-center bg-dark-200-">
+          <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8ff" />
+          <TextInput
+              onPress={() => {}}
+              placeholder="Search"
+              value=""
+              onChangeText={() => {}}
+              placeholderTextColor="#ab8ff"
+              className="flex-1 ml-2 text-white"
+
+          />
+        </View>
+    );
+}
